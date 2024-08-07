3635d89abd3df506bda3da11bc297b9c903eec9b
Kayla Gray <kjoygray75@gmail.com>
2024-08-07 12:51:54 -0500

    Added new() function for Player and added player to main function

--- src/player.rs
+++ src/player.rs
@@ -3,6 +3,10 @@ use crate::event::DamageKind;
 pub struct Player {}
 
 impl Player {
+    pub fn new() -> Self {
+        Self {}
+    }
+
     pub fn apply_damage(&mut self, kind: DamageKind) {
         todo!()
     }

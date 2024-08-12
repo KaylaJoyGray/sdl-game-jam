389a4daea494132634c815a979e1fd572df998ca
Kayla Gray <kjoygray75@gmail.com>
2024-08-12 12:01:01 -0500

    Switched from SDL Surface to GPU Texture

--- src/background.rs
+++ src/background.rs
@@ -3,19 +3,22 @@
  *  Scrolling background
  *
  ****************************************/
+use sdl2::image::LoadTexture;
 use sdl2::rect::Rect;
-use sdl2::surface::{Surface, SurfaceRef};
+use sdl2::render::{Texture, TextureCreator, WindowCanvas};
+use sdl2::surface::SurfaceRef;
+use sdl2::video::WindowContext;
 
 pub struct Background<'a> {
     pub speed: f32,
     index: usize,
-    surfaces: Vec<Surface<'a>>,
+    textures: Vec<Texture<'a>>,
 }
 
 impl<'a> Background<'a> {
-    pub fn new() -> Self {
+    pub fn new(texture_creator: &'a TextureCreator<WindowContext>) -> Self {
         let mut sfx = 0;
-        let mut surfaces: Vec<Surface<'a>> = Vec::new();
+        let mut textures: Vec<Texture<'a>> = Vec::new();
 
         while sfx < 40 {
             let mut path: String = "assets/background/00".to_string();
@@ -26,7 +29,7 @@ impl<'a> Background<'a> {
             path.push_str(&sfx_str);
 
             let error = format!("Error: could not load image: {}", path);
-            surfaces.push(Surface::load_bmp(path).expect(&error));
+            textures.push(texture_creator.load_texture(path).expect(&error));
 
             sfx += 1;
         }
@@ -34,7 +37,7 @@ impl<'a> Background<'a> {
         Self {
             speed: 0.1,
             index: 0,
-            surfaces,
+            textures,
         }
     }
 
@@ -42,23 +45,29 @@ impl<'a> Background<'a> {
         todo!()
     }
 
-    pub fn blit(&mut self, screen_surface: &mut SurfaceRef, screen_width: u32, screen_height: u32, delta: f32) {
+    pub fn blit(
+        &mut self,
+        canvas: &mut WindowCanvas,
+        screen_width: u32,
+        screen_height: u32,
+        delta: f32,
+    ) {
         // update index
         let add = (delta % self.speed) as usize;
-        self.index = (self.index + add) % self.surfaces.len();
+        self.index = (self.index + add) % self.textures.len();
 
         let mut y: i32 = (0 - (screen_height / 2)) as i32;
         let mut x: i32 = (0 - (screen_width / 2)) as i32;
         while y < (screen_height / 2) as i32 {
             while x < (screen_width / 2) as i32 {
-                self.surfaces[self.index]
-                    .blit(
-                        Rect::new(0, 0, 128, 128),
-                        screen_surface,
+                canvas
+                    .copy(
+                        &self.textures[self.index],
+                        None,
                         Rect::new(x, y, 1920, 1080),
                     )
                     .expect("Error: could not draw background");
-                x += 128;   // tile size
+                x += 128; // tile size
             }
             y += 128;
             x = (0 - (screen_width / 2)) as i32;

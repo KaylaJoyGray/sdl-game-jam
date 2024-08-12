b5ee1539410078198c810297a5e91daeba215e5e
Kayla Gray <kjoygray75@gmail.com>
2024-08-12 10:23:40 -0500

    Updates background module and image

--- src/background.rs
+++ src/background.rs
@@ -3,16 +3,34 @@
  *  Scrolling background
  *
  ****************************************/
-use sdl2::surface::Surface;
+use sdl2::rect::Rect;
+use sdl2::surface::{Surface, SurfaceRef};
+use sdl2::sys::SDL_Surface;
+use std::ops::Deref;
 
-struct Background<'a> {
-    speed: f32,
-    surface: &'a Surface<'a>,
+pub struct Background<'a> {
+    pub speed: f32,
+    surface: Surface<'a>,
 }
 
-impl<'a> Background<'a>  {
+impl<'a> Background<'a> {
+    pub fn new(path: &str) -> Self {
+        let surface = Surface::load_bmp(path);
+        Self {
+            speed: 0.1,
+            surface: surface.expect("Error: could not load background image"),
+        }
+    }
 
-    fn present(&self) {
+    pub fn update() {
         todo!()
+    }
+
+    pub fn blit(&self, screen_surface: &mut SurfaceRef) {
+        self.surface.blit(
+            Rect::new(0, 0, 1920, 1080),
+            screen_surface,
+            Rect::new(0, 0, 1920, 1080),
+        );
     }
 }
